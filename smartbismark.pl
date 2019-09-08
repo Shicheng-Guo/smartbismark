036b68e66cbf5c73dbd0acbccefb2a665da9dbe8
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-08 17:17:59 -0500

    Update smartbismark.pl

--- smartbismark.pl
+++ smartbismark.pl
@@ -4,18 +4,21 @@
 # Contact: Shicheng Guo
 # Version 1.3
 # Update: 09/08/2019
+# smartbismark.pl 
+# USAGE: smartbismark --input saminfo.txt --genome hg19 --server TSCC/MCRI --submit
+
 
 use strict;
 use Cwd;
 use Getopt::Long;
 my $smartbismark_version="v0.16.3_dev";
 my $dir=getcwd;
-my ($input,$genome,$server,$BismarkRefereDb,$queue,$help,$submit,$nodes,$ppn,$walltime,$multicore)=process_command_line();
+my ($input,$genome,$server,$BismarkRefereDb,$queue,$phred,$help,$submit,$nodes,$ppn,$walltime,$multicore)=process_command_line();
 my $sed=directory_build();
 
-open F,$input;
+open SRR,$input;
 my %SRR; my %SRA;
-while(<F>){
+while(<SRR>){
     chomp;
     next if /^\s+$/;
 	next if /sample_accession/i;
@@ -29,97 +32,85 @@ while(<F>){
 		push @{$SRA{$SRX}},$SRR;
 		}
 	}	
-    my $predownload=system("fastq-dump --skip-technical -X 8000 --split-files --gzip $SRR");
-    my @read = glob("$SRR*fastq*gz");
-    my $phred;
-    if($server eq "GM"){
-    chomp(my $phredcheck=`perl /media/Home_Raid1/shg047/bin/checkphred.pl $read[0]`);
-    ($phred)=split /\s+/,$phredcheck; 
-    $phred=33 if ! defined $phred;
-    print "$read[0]\tphred= $phred\n";
-    }elsif($server eq "TSCC"){
-    chomp(my $phredcheck=`perl ~/bin/checkphred.pl $read[0]`);
-    ($phred)=split /\s+/,$phredcheck; 
-    $phred=33 if ! defined $phred; 
-    print "$read[0]\tphred= $phred\n";
-    }
-    my $chrLenhg19="/home/shg047/oasis/db/hg19/hg19.chrom.sizes";
-    my $cpgPoshg19="/home/shg047/oasis/db/hg19/HsGenome19.CpG.positions.txt"; 
-    my $curr_dir = $dir;
-    if(scalar(@read) eq 2){
-    my($sample,undef)=split /_1.fastq.gz|_R1.fastq.gz/,$read[0]; 	
-    my($sample1,undef)=split /.fastq.gz/,$read[0];
-    my($sample2,undef)=split /.fastq.gz/,$read[1];
-    my $job_file_name = "$SRR.pbs";
-    open(OUT, ">$job_file_name") || die("Error in opening file $job_file_name.\n");   
+close SRR;
+	
+my @read = glob("$SRR*fastq*gz");
+my $chrLenhg19="~/hpc/db/hg19/hg19.chrom.sizes";
+my $cpgPoshg19="~/hpc/db/hg19/HsGenome19.CpG.positions.txt"; 
+my $curr_dir = $dir;
+if(scalar(@read) eq 2){
+my($sample,undef)=split /_1.fastq.gz|_R1.fastq.gz/,$read[0]; 	
+my($sample1,undef)=split /.fastq.gz/,$read[0];
+my($sample2,undef)=split /.fastq.gz/,$read[1];
+
+my $job_file_name = "$SRR.pbs";
+open(OUT, ">$job_file_name") || die("Error in opening file $job_file_name.\n");   
     
-    print OUT "#!/bin/csh\n";
-    print OUT "#PBS -N $sample\n";
-    print OUT "#PBS -q $queue\n";  # glean is free
-    print OUT "#PBS -l nodes=$nodes:ppn=$ppn\n";
-    print OUT "#PBS -l walltime=$walltime\n";
-    print OUT "#PBS -o $sample.log\n";
-    print OUT "#PBS -e $sample.err\n";
-    print OUT "#PBS -V\n";
-    print OUT "#PBS -M shihcheng.guo\@gmail.com \n";
-    print OUT "#PBS -m abe\n";
-    print OUT "cd $curr_dir\n";    
+print OUT "#!/bin/csh\n";
+print OUT "#PBS -N $sample\n";
+print OUT "#PBS -q $queue\n";  # glean is free
+print OUT "#PBS -l nodes=$nodes:ppn=$ppn\n";
+print OUT "#PBS -l walltime=$walltime\n";
+print OUT "#PBS -o $sample.log\n";
+print OUT "#PBS -e $sample.err\n";
+print OUT "#PBS -V\n";
+print OUT "#PBS -M shihcheng.guo\@gmail.com \n";
+print OUT "#PBS -m abe\n";
+print OUT "cd $curr_dir\n";    
 
-    my $extractor="bismark_methylation_extractor --no_overlap --multicore $multicore --merge_non_CpG --bedGraph --cutoff 5 --ignore 1 --buffer_size 4G";
-    #my $bismark="bismark --bowtie2 --non_directional --multicore $multicore --fastq -N 1"; 
-    my $bismark="bismark --bowtie2 --multicore $multicore --fastq -N 1"; 
-    my $coverage2cytosine="coverage2cytosine --merge_CpG --gzip --genome_folder";
-    print OUT "fastq-dump --skip-technical --split-files --gzip $SRR\n";
-    print OUT "trim_galore --paired --phred$phred --clip_R1 3 --clip_R2 6 --fastqc --illumina $sample1\.fastq.gz $sample2\.fastq.gz --output_dir ../fastq_trim\n";
-    print OUT "$bismark --phred$phred-quals $BismarkRefereDb -1 ../fastq_trim/$sample1\_val_1.fq.gz -2 ../fastq_trim/$sample2\_val_2.fq.gz -o ../bam\n";
-    print OUT "filter_non_conversion --paired ../bam/$sample1\_val_1_bismark_bt2_pe.bam\n";
-    print OUT "deduplicate_bismark --bam ../bam/$sample1\_val_1_bismark_bt2.nonCG_filtered.bam\n";   
-    print OUT "samtools sort -@ 8 ../bam/$sample1\_val_1_bismark_bt2.nonCG_filtered.deduplicated.bam -o ../sortbam/$sample\_bismark_bt2_pe.sortc.bam\n";
-    print OUT "samtools index ../sortbam/$sample\_bismark_bt2_pe.sortc.bam\n";
-    print OUT "cd ../sortbam\n";
-    print OUT "perl ~/bin/samInfoPrep4Bam2Hapinfo.pl ~/oasis/db/hg19/hg19.cut10k.bed > saminfo.txt\n";
-    print OUT "perl ~/bin/bam2hapInfo2PBS.pl saminfo.txt submit bismark $chrLenhg19 $cpgPoshg19\n";
-    print OUT "$extractor --comprehensive --output ../methyfreq  ../bam/$sample1\_val_1_bismark_bt2.nonCG_filtered.deduplicated.bam\n";
-    print OUT "$coverage2cytosine $BismarkRefereDb -o $sample1.mergeCpG.bed $sample1\_val_1_bismark_bt2_pe.nonCG_filtered.bismark.cov.gz\n";
-    }elsif(scalar(@read) == 1){	
-    my($sample,undef)=split /_1.fastq.gz|_R1.fastq.gz/,$read[0]; 	
-    my($sample1,undef)=split /.fastq.gz/,$read[0];
-    my $job_file_name = "$SRR.pbs";
-    open(OUT, ">$job_file_name") || die("Error in opening file $job_file_name.\n");  
-    my $extractor="bismark_methylation_extractor --single-end --merge_non_CpG --bedGraph --cutoff 5 --ignore 1 --buffer_size 4G";
-    print OUT "#!/bin/csh\n";
-    print OUT "#PBS -N $sample\n";
-    print OUT "#PBS -q $queue\n";  # glean is free
-    print OUT "#PBS -l nodes=$nodes:ppn=$ppn\n";
-    print OUT "#PBS -l walltime=$walltime\n";
-    print OUT "#PBS -o $sample.log\n";
-    print OUT "#PBS -e $sample.err\n";
-    print OUT "#PBS -V\n";
-    print OUT "#PBS -M shihcheng.guo\@gmail.com \n";
-    print OUT "#PBS -m abe\n";
-    print OUT "#PBS -A k4zhang-group\n";
-    print OUT "cd $curr_dir\n";  
-    print OUT "fastq-dump --skip-technical --split-files --gzip $sample\n";
-    print OUT "trim_galore --phred$phred --fastqc --illumina $sample1.fastq.gz --output_dir ../fastq_trim\n";
-    print OUT "bismark --bowtie2 --phred$phred-quals --fastq -N 1 --multicore $multicore $BismarkRefereDb ../fastq_trim/$sample1\_trimmed.fq.gz -o ../bam\n";  
-    print OUT "filter_non_conversion --single ../bam/$sample1\_trimmed_bismark_bt2.bam\n"; 
-    print OUT "deduplicate_bismark --bam ../bam/$sample1\_trimmed_bismark_bt2.nonCG_filtered.bam\n";
-    print OUT "$extractor --comprehensive --output ../methyfreq  ../bam/$sample1\_trimmed_bismark_bt2.nonCG_filtered.deduplicated.bam\n";
-    print OUT "samtools sort ../bam/$sample1\_trimmed_bismark_bt2.nonCG_filtered.deduplicated.bam -o ../sortbam/$sample.bismark_bt2_se.sort.bam\n";
-    print OUT "samtools index ../sortbam/$sample.bismark_bt2_se.sort.bam\n";
-    print OUT "perl ~/bin/samInfoPrep4Bam2Hapinfo.pl ~/oasis/db/hg19/hg19.cut10k.bed > saminfo.txt\n";
-    print OUT "perl ~/bin/bam2hapInfo2PBS.pl saminfo.txt submit bismark $chrLenhg19 $cpgPoshg19\n";
-    }
-
-   close(OUT);
-   
-   if($submit eq 'submit'){
-    system("qsub $SRR.pbs");
-    }else{
-    print "qsub $SRR.pbs\n";
-    }
+my $extractor="bismark_methylation_extractor --no_overlap --multicore $multicore --merge_non_CpG --bedGraph --cutoff 5 --ignore 1 --buffer_size 4G";
+my $bismark="bismark --bowtie2 --multicore $multicore --fastq -N 1"; 
+my $coverage2cytosine="coverage2cytosine --merge_CpG --gzip --genome_folder";
+    
+print OUT "fastq-dump --skip-technical --split-files --gzip $SRR\n";
+print OUT "trim_galore --paired --phred$phred --clip_R1 3 --clip_R2 6 --fastqc --illumina $sample1\.fastq.gz $sample2\.fastq.gz --output_dir ../fastq_trim\n";
+print OUT "$bismark --phred$phred-quals $BismarkRefereDb -1 ../fastq_trim/$sample1\_val_1.fq.gz -2 ../fastq_trim/$sample2\_val_2.fq.gz -o ../bam\n";
+print OUT "filter_non_conversion --paired ../bam/$sample1\_val_1_bismark_bt2_pe.bam\n";
+print OUT "deduplicate_bismark --bam ../bam/$sample1\_val_1_bismark_bt2.nonCG_filtered.bam\n";   
+print OUT "samtools sort -@ 8 ../bam/$sample1\_val_1_bismark_bt2.nonCG_filtered.deduplicated.bam -o ../sortbam/$sample\_bismark_bt2_pe.sortc.bam\n";
+print OUT "samtools index ../sortbam/$sample\_bismark_bt2_pe.sortc.bam\n";
+print OUT "cd ../sortbam\n";
+print OUT "perl ~/bin/samInfoPrep4Bam2Hapinfo.pl ~/oasis/db/hg19/hg19.cut10k.bed > saminfo.txt\n";
+print OUT "perl ~/bin/bam2hapInfo2PBS.pl saminfo.txt submit bismark $chrLenhg19 $cpgPoshg19\n";
+print OUT "$extractor --comprehensive --output ../methyfreq  ../bam/$sample1\_val_1_bismark_bt2.nonCG_filtered.deduplicated.bam\n";
+print OUT "$coverage2cytosine $BismarkRefereDb -o $sample1.mergeCpG.bed $sample1\_val_1_bismark_bt2_pe.nonCG_filtered.bismark.cov.gz\n";
+}elsif(scalar(@read) == 1){
+	my($sample,undef)=split /_1.fastq.gz|_R1.fastq.gz/,$read[0]; 	
+	my($sample1,undef)=split /.fastq.gz/,$read[0];
+	my $job_file_name = "$SRR.pbs";
+	open(OUT, ">$job_file_name") || die("Error in opening file $job_file_name.\n");  
+	my $extractor="bismark_methylation_extractor --single-end --merge_non_CpG --bedGraph --cutoff 5 --ignore 1 --buffer_size 4G";
+	print OUT "#!/bin/csh\n";
+	print OUT "#PBS -N $sample\n";
+	print OUT "#PBS -q $queue\n";
+	print OUT "#PBS -l nodes=$nodes:ppn=$ppn\n";
+	print OUT "#PBS -l walltime=$walltime\n";
+	print OUT "#PBS -o $sample.log\n";
+	print OUT "#PBS -e $sample.err\n";
+	print OUT "#PBS -V\n";
+	print OUT "#PBS -M shihcheng.guo\@gmail.com \n";
+	print OUT "#PBS -m abe\n";
+	print OUT "#PBS -A k4zhang-group\n";
+	print OUT "cd $curr_dir\n";  
+	print OUT "fastq-dump --skip-technical --split-files --gzip $sample\n";
+	print OUT "trim_galore --phred$phred --fastqc --illumina $sample1.fastq.gz --output_dir ../fastq_trim\n";
+	print OUT "bismark --bowtie2 --phred$phred-quals --fastq -N 1 --multicore $multicore $BismarkRefereDb ../fastq_trim/$sample1\_trimmed.fq.gz -o ../bam\n";  
+	print OUT "filter_non_conversion --single ../bam/$sample1\_trimmed_bismark_bt2.bam\n"; 
+	print OUT "deduplicate_bismark --bam ../bam/$sample1\_trimmed_bismark_bt2.nonCG_filtered.bam\n";
+	print OUT "$extractor --comprehensive --output ../methyfreq  ../bam/$sample1\_trimmed_bismark_bt2.nonCG_filtered.deduplicated.bam\n";
+	print OUT "samtools sort ../bam/$sample1\_trimmed_bismark_bt2.nonCG_filtered.deduplicated.bam -o ../sortbam/$sample.bismark_bt2_se.sort.bam\n";
+	print OUT "samtools index ../sortbam/$sample.bismark_bt2_se.sort.bam\n";
+	print OUT "perl ~/bin/samInfoPrep4Bam2Hapinfo.pl ~/oasis/db/hg19/hg19.cut10k.bed > saminfo.txt\n";
+	print OUT "perl ~/bin/bam2hapInfo2PBS.pl saminfo.txt submit bismark $chrLenhg19 $cpgPoshg19\n";
 }
-
+close(OUT);
+ 
+if($submit eq 'submit'){
+	system("qsub $SRR.pbs");
+}else{
+	print "qsub $SRR.pbs\n";
+}
+}
 
 sub process_command_line{
 	my $help;
@@ -131,23 +122,23 @@ sub process_command_line{
 	my %ppn;
 	my %multicore;
 	my $nodes;
-        my $BismarkRefereDb;
+    my $BismarkRefereDb;
     
 	my $command_line=GetOptions ( 
-                                  "input=s"    		        => \$input,
+                                  "input=s"    		    => \$input,
                                   "genome=s"   			=> \$genome,
                                   "server=s"   			=> \$server,
-	                          "queue=s"   			=> \$queue,                                                                    
+	                              "queue=s"   			=> \$queue,                                                                    
                                   "help"      			=> \$help,
-	                          "submit=s"   			=> \$submit,
-	                          "BismarkRefereDb=s" 		=> \$BismarkRefereDb,
+	                              "submit=s"   			=> \$submit,
+	                              "BismarkRefereDb=s"   => \$BismarkRefereDb,
+	                              "phred=s"             => \$phred,
 	                             );
 	
     unless (defined $genome){
     warn "\n\n\t[Error]: Please respecify essential command line options!\n";
     print_helpfile();
 	}
-    
     #################################################################################################
     ##################### SmartBismark Version and Usage (Version and Usage) ########################
     #################################################################################################
@@ -191,11 +182,26 @@ sub process_command_line{
 		}else{
 		warn("Please assign genome version (in Genome-miner)to the script: hg19? hg38? mm9? mm10?");	
 		}
+	}elsif($server eq "MCRI"){
+		if($genome eq "hg19"){
+		$BismarkRefereDb="~/hpc/db/hg19/bismark";
+		print "Bismark alignment reference: BismarkRefereDb\n";
+		}elsif($genome=="hg38"){
+		$BismarkRefereDb="/home/shg047/db/hg38/bismark/";
+		print "Bismark alignment reference: BismarkRefereDb\n";
+		}elsif($genome eq "mm9"){
+		$BismarkRefereDb="/home/shg047/db/mm9/bismark/";
+		print "Bismark alignment reference: BismarkRefereDb\n";
+		}elsif($genome eq "mm10"){
+    	        $BismarkRefereDb="/home/shg047/db/mm10/bismark/";
+				print "Bismark alignment reference: BismarkRefereDb\n";
+		}else{
+				warn("Please assign genome version (in Genome-miner)to the script: hg19? hg38? mm9? mm10?");	
+		}
 	}else{
-		print "Please check the server name or build corresponding reference for server:$server\n";
+        print "Please check the server name or build corresponding reference for server:$server\n";
 	}
-    }
-    
+}
     #################################################################################################
     ##################### Assemble PBS Paramters (nodes, ppn, walltime multicore) ##########################
     #################################################################################################
@@ -227,9 +233,7 @@ sub process_command_line{
     $ppn=$ppn{$queue};
     $walltime=$walltime{$queue};
     $multicore=$multicore{$queue};
-    #################################################################################################
-    ##################### Return All the paramters for SmartBismark ##########################
-    #################################################################################################
+
     return($input,$genome,$server,$BismarkRefereDb,$queue,$help,$submit,$nodes,$ppn,$walltime,$multicore);
 }
 
@@ -253,7 +257,7 @@ sub print_helpfile{
 	           erence will be chosed. if you have new reference rather than huamn and mouse, please
 	           creat the methylation alignment reference advanced and you can contact me to do that. 
 	
-	--server   TSCC, GM(GenomeMiner). Combined with genome, --server and --genome will provide the 
+	--server   TSCC, GM(GenomeMiner),MCRI. Combined with genome, --server and --genome will provide the 
 	           location of the alignment reference for bismark. 
 	           
 	--submit   Submit pbs job or not. SmartBismark will creat pbs job files for each fastq file and
@@ -261,7 +265,6 @@ sub print_helpfile{
 	           submitted and PBS ID will be printed in the STANDOUT.    
 	--queue    TSCC queue: hotel, glean, pdafm, condo
 			   
-	
 HOW_TO
 exit;
 }
@@ -296,7 +299,6 @@ mkdir "./tmp/" if ! -e "./tmp/"               || print  "tmp            Building
 mkdir "./hapinfo/" if ! -e "./hapinfo/"       || print  "hapinfo        Building Succeed!  <hapinfo Files>    stored here\n";
 }
 
-
 sub bismark_version{
 	my @version=`bismark --version`;
 	my $bismark_version;
@@ -307,4 +309,3 @@ sub bismark_version{
 	}
 	return($bismark_version);
 }
-
